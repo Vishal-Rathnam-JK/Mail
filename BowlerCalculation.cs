22fb2ffd99e0eed6bfe451ee1a20eb503b367292
Vishal-Rathnam-JK <32157113+Vishal-Rathnam-JK@users.noreply.github.com>
2018-09-07 18:14:22 +0530

    sent to ram sir for juniors to use

---
+++ BowlerCalculation.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringManipulation_BowlerEconomyAndStrikerate
+{
+    class BowlerCalculation
+    {
+        public static void Main()
+        {
+            float oversBowled;
+            string deliveryInformation;
+            float wicketsTaken = 0;
+            float ballsBowled = 0;
+            string reset = "none";
+            float runsScored = 0;
+            double economyRate;
+            double strikeRate;           
+            do
+            {
+                reset = "none";
+                Console.Write("Enter the number of overs bowled :");
+                oversBowled = float.Parse(Console.ReadLine());
+                if (oversBowled < 0 || (oversBowled > 0.5 && oversBowled < 1) || oversBowled > 1.5)
+                {
+                    reset = "repeat";
+                }
+            } while (reset=="repeat");
+            string[] oversArray = Convert.ToString(oversBowled).Split('.');
+            ballsBowled = int.Parse(oversArray[0]) * 6 + int.Parse(oversArray[1]);
+            Console.Write("Enter the runs scored on each ball , seperated by commas");         
+            do
+            {
+                reset = "none";
+               deliveryInformation = Console.ReadLine();
+               string[] deliveryArray = deliveryInformation.Split(',');
+                if (deliveryArray.Count()!=ballsBowled)
+                {
+                    reset = "repeat";
+                }
+                
+                foreach (string item in deliveryArray)
+                {
+                    if (item=="W" || item == "w")
+                    {
+                        wicketsTaken++;  
+                    }
+                    else if (int.Parse(item) < 7)
+                    {
+                        runsScored = runsScored + int.Parse(item);
+                    }
+                    else
+                    {
+                        reset = "repeat";
+                    }  
+                }
+            } while (reset=="repeat");
+            economyRate = runsScored * 6 / ballsBowled;
+            strikeRate = wicketsTaken / ballsBowled;
+                Console.WriteLine("The Economy Rate = {0} \n Strike Rate = {1}", economyRate,strikeRate);                         
+        }
+    }
+}
